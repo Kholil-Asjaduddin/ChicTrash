8a1b720e338be8f6c9a4819f513e2b0022494e25
Asoatram <muhamaddaffaazfarabbani@mail.ugm.ac.id>
2024-08-26 14:49:44 +0700

    Batman

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Program{
+
+   
+    public static void Main(String[] Args){
+        Console.WriteLine("Halo nama kamu siapa?");
+        String name = Console.ReadLine();
+        Console.WriteLine($"Halo {name}");
+    }
+
+}
