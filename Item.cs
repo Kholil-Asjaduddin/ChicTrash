9ddcfd94dd786905f5dc5e8b1c1672886fbff854
Kholil-Asjaduddin <kholilasjaduddin@mail.ugm.ac.id>
2024-09-06 16:05:04 +0700

    feat: add item class

---
+++ Item.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChicTrash
+{
+    class Item
+    {
+        //Properties
+        private string _itemName;
+        private string _category;
+        private string _description;
+        private double _price;
+        private int _quantity;
+
+        public string ItemName
+        {
+            get { return _itemName; }
+            set { _itemName = value; }
+        }
+        public string Category
+        {
+            get { return _category; }
+            set { _category = value; }
+        }
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+        public double Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+        public int Quantity
+        {
+            get { return _quantity; }
+            set { _quantity = value; }
+        }
+    }
+}
