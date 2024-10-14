aa8bd3e8c6391962f9a60c9f3989ce42efb88b4a
Kholil-Asjaduddin <kholilasjaduddin@mail.ugm.ac.id>
2024-10-14 15:09:57 +0700

    add model 7

---
+++ Module 7/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChicTrash.Module_7
+{
+    public class Employee
+    {
+        private int _employeeId;
+        private string _loginName;
+        private string _password;
+
+        public string LoginName
+        {
+            get { return _loginName; }
+            set { _loginName = value; }
+        }
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+        public int EmployeeID
+        {
+            get { return _employeeId; }
+            set { _employeeId = value; }
+        }
+
+        public bool Login(string loginName, string password)
+        {
+            if (loginName != null && password != null) 
+                return true;
+            else
+                return false;
+        }
+    }
+}
