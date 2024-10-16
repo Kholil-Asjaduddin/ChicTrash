cb16b5f262265cf4462da53a2993a354524ee399
Muhamad Daffa Azfa Rabbani <109969988+Asoatram@users.noreply.github.com>
2024-10-16 21:15:23 +0700

    chore: reorganizing file

---
+++ Class/Item.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChicTrash
+{
+    public class Item
+    {
+        //Properties
+        private int _itemId;
+        private string _itemName;
+        private string _category;
+        private string _description;
+        private double _price;
+        private int _quantity;
+
+        public int ItemId
+        {
+            get { return _itemId; }
+            set { _itemId = value; }
+        }
+        public string ItemName
+        {
+            get { return _itemName; }
+            set { _itemName = value; }
+        }
+        public string Category
+        {
+            get { return _category; }
+            set { _category = value; }
+        }
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+        public double Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+        public int Quantity
+        {
+            get { return _quantity; }
+            set { _quantity = value; }
+        }
+    }
+}
---
+++ Class/User.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChicTrash
+{
+    public class User
+    {
+        //Properties
+        private int _userId;
+        private string _userName;
+        private string _userEmail;
+        private string _userPassword;
+        private string _userPhone;
+        private string _userAdress;
+        private double _userMoney;
+        
+
+        public string UserName
+        {
+            get { return _userName; }
+            set { _userName = value; }
+        }
+        public string UserEmail
+        {
+            get { return _userEmail; }
+            set { _userEmail = value; }
+        }
+
+        public string UserPassword
+        {
+            get { return _userPassword; }
+            set { _userPassword = value; }
+        }
+        public string UserPhone
+        {
+            get { return _userPhone; }
+            set { _userPhone = value; }
+        }
+        public double UserMoney
+        {
+            get { return _userMoney; }
+            set { _userMoney = value; }
+        }
+        public string UserAdress
+        {
+            get { return _userAdress; }
+            set { _userAdress = value; }
+        }
+
+        public int UserUserId
+        {
+            get { return _userId; }
+            set { _userId = value; }
+        }
+
+        //Method
+        public Boolean Login(string userName, string userPassword)
+        {
+            if (userName == "hoho" & userPassword == "000")
+            {
+                _userId = 1;
+                return true;
+            }
+            else if (userName == "kholil" & userPassword == "111")
+            {
+                _userId = 2;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public double GetMoney()
+        {
+            return _userMoney;
+        }
+
+        public void Register()
+        {
+            _userName = "Dapa";
+            _userId = 10;
+            _userAdress = "Pogung";
+            _userEmail = "Dapa123@mail.com";
+            _userPassword = "password";
+            _userPhone = "08222222222222";
+        }
+    }
+}
